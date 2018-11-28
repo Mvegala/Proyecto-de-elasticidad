a46c758eb5d5cba68f48f4910bd629af9fc0ff97
Marilin <mvegala@gmail.com>
2018-11-28 09:16:43 +0100

    se añadieron las formulas de calculo de probabilidad de visitas y metodo para saber los adyacentes

--- main.py
+++ main.py
@@ -31,16 +31,15 @@ B = [
     [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
 ]
 
-dfB = pd.DataFrame(A, index=states, columns=states)
-
+dfB = pd.DataFrame(B, index=states, columns=states)
 
 # visits data
 v_data = [23, 56, 11, 678, 6, 95, 111, 34]
 V = pd.Series(v_data, index=states)
 
+
 # ----------------------------------------------------
-
-
+# hay que modificarlo
 def Vx(V, M, state):
     # get visits data
     vs = V.values
@@ -52,10 +51,90 @@ def Vx(V, M, state):
 
     return vx_value
 
+
 # ----------------------------------------------------
 
+def VA(M):
+    # para matriz A
+    peh = Px(dfA, 'e', 'h')
+    phs = Px(dfA, 'h', 's')
+    pss = Px(dfA, 's', 's')
+    psv = Px(dfA, 's', 'v')
+    phg = Px(dfA, 'h', 'g')
+    psg = Px(dfA, 's', 'g')
+    pvg = Px(dfA, 'v', 'g')
+    pgc = Px(dfA, 'g', 'c')
+    pgb = Px(dfA, 'g', 'b')
+
+    # a = np.array([[Ve,peh], [Vh,phs,Vs,pss]])
+    # b = np.array([Vh,Vs])
+    # x = np.linalg.solve(a,b)
+
+    Ve = 1
+    Vh = Ve * peh
+    # Vs = Vh * phs + Vs * pss
+    Vs = (Vh * phs) / (1 - pss)
+    Vv = Vs * psv
+    Vg = Vh * phg + Vs * psg + Vv * pvg
+    Vc = Vg * pgc
+    Vb = Vg * pgb
+
+    return (Ve, Vh, Vs, Vv, Vg, Vc, Vb)
+
+
+# ----------------------------------------------------
+# para matriz B
+def VB(M):
+    peh = Px(dfB, 'e', 'h')
+    peh = Px(dfB, 'e', 'h')
+    phs = Px(dfB, 'h', 's')
+    pss = Px(dfB, 's', 's')
+    psv = Px(dfB, 's', 'v')
+    phg = Px(dfB, 'h', 'g')
+    psg = Px(dfB, 's', 'g')
+    pvg = Px(dfB, 'v', 'g')
+    pgc = Px(dfB, 'g', 'c')
+    pgb = Px(dfB, 'g', 'b')
+
+    # a = np.array([[Ve,peh], [Vh,phs,Vs,pss]])
+    # b = np.array([Vh,Vs])
+    # x = np.linalg.solve(a,b)
+
+    Ve = 1
+    Vh = Ve * peh
+    # Vs = Vh * phs + Vs * pss
+    Vs = (Vh * phs) / (1 - pss)
+    Vv = Vs * psv
+    Vg = Vh * phg + Vs * psg + Vv * pvg
+    Vc = Vg * pgc
+    Vb = Vg * pgb
+
+    # para matriz B
+
+    return (Ve, Vh, Vs, Vv, Vg, Vc, Vb)
+
+
+# ----------------------------------------------------
+# get visit probabilities for given state global
+
+def Px(M, state1, state2):
+    px = M.at[state1, state2]
+    return px
+
+
+# -----------------------------------------------------
+# get adyacent to me
+
+def Ady(M, state):
+    ad = dfA[dfA[state] > 0]
+    return ad
+
+
+# -----------------------------------------------------
 
 if __name__ == '__main__':
     s_value = Vx(V, dfA, 's')
 
-    print(s_value)
+    print(VA(dfA))
+    print(VB(dfB))
+
