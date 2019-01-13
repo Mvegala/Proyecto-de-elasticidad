3447dade71c970a75610eefebacff0ffd6b67f79
iv@n <iv@n.com>
2019-01-13 21:41:26 +0100

    Revisión trabajo - terminado; falta revisar y subir

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 
 import numpy as np
 import pandas as pd
+import time, random
+import matplotlib.pyplot as plt
 
 # ----------------------------------------------------
 #matriz de probabilidado de transicion A
@@ -155,6 +157,13 @@ def Rx(i, N, gamma):
         rx += dem / (1 - Ux(es, N, gamma))
     return rx
 
+def RxN(i, Ns, gamma):
+    rx = 0
+    for es in statesD:
+        dem = Px(dfD, es, i)
+        rx += dem / (1 - Ux(es, Ns[i], gamma))
+    return rx
+
 #-----------------------------------------------------
 
 
@@ -165,11 +174,272 @@ def cargaPorGamma(gamma, N):
         rx.append((i, Rx(i, N, gamma)))
     return rx
 
+def cargaPorGammaAndNumComponentes(gamma, Ns):
+    rx = []
+    for i in statesE:
+        rx.append((i, RxN(i, Ns, gamma)))
+    return rx
+
+
+
+
+
+
+
+def grafico(filas, columnas):
+    import random
+    # ysample = random.sample(range(-50, 50), 100)
+    #
+    # xdata = []
+    # ydata = []
+
+    plt.show()
+    fig, axes = plt.subplots(filas, columnas, sharex='all', sharey='all')
+    valores = []
+
+    for i in range(filas):
+        valores.append([])
+        for j in range(columnas):
+            valores[i].append({'x': [], 'y': [], 'nComp': 0})
+            line, = axes[i, j].plot([], [], 'r-')
+
+    # for a in range(100):
+    #
+    #     xdata.append(a)
+    #     ydata.append(ysample[a])
+    #     for i in range(2):
+    #     #         for j in range(3):
+    #     #             line, = axes[i, j].plot(xdata, ydata, 'r-')
+    #     #             line.set_xdata(xdata)
+    #     #             line.set_ydata(ydata)
+    #     #     plt.draw()
+    #     #     plt.pause(1e-17)
+    #     #     time.sleep(0.1)
+
+    # add this if you don't want the window to disappear at the end
+    # plt.show()
+    return fig, axes, valores
+
+
+
+
+
+def actualizaGrafico(filas, columnas, axes, valores):
+    for i in range(filas):
+        for j in range(columnas):
+            line, = axes[i, j].plot(valores[i][j]['x'], valores[i][j]['y'], 'r-')
+            axes[i, j].set_title(f'C={valores[i][j]["nComp"]} g={valores[i][j]["gamma"]}')
+
+            x = valores[i][j]['x']
+            y = valores[i][j]['y']
+            if len(x) > 12:
+                x = [x[-10:]]
+                y = [y[-10:]]
+
+            line.set_xdata(x)
+            line.set_ydata(y)
+    plt.draw()
+    plt.pause(1e-17)
+
+
+
+
+
+
+
+def tRespMaxMin():
+    fig, axes, valores = grafico(2, 3)
+    N = 1
+    g = 0
+    maximo = 25
+    minimo = 10
+    iteracion = -1 #Iteración actual
+    incGamma = 0.2
+    contadorIguales = {}
+    for el in statesE:
+        contadorIguales[el] = 0
+    while iteracion < 1000:
+        incGamma = random.random() * 0.5
+
+
+        iteracion += 1
+        if iteracion > 100 and iteracion < 200:
+            incGamma = -1 * incGamma
+        if iteracion > 400 and iteracion < 600:
+            incGamma = -1 * incGamma
+        if iteracion > 900 and iteracion < 1000:
+            incGamma = -1 * incGamma
+        if g + incGamma < 0:
+            incGamma = 0
+
+        g += incGamma
+
+        error = False
+        # cargas = cargaPorGammaAndNumComponentes(g, Ns)
+        cargas = cargaPorGamma(g, N)
+        accion = {'mas': 0, 'menos': 0}
+        for indElemento, a in enumerate(cargas):
+            carga = round(a[1]*100, 2)
+            if indElemento > 2:
+                i = 1
+                j = indElemento-3
+            else:
+                i = 0
+                j = indElemento
+
+            valores[i][j]['x'].append(iteracion)
+            valores[i][j]['y'].append(carga)
+            # valores[i][j]['nComp'] = Ns[a[0]]
+            valores[i][j]['nComp'] = N
+            valores[i][j]['gamma'] = str(round(g, 2)) + f' n={a[0]} K={carga}'
+            # print(valores)
+
+
+            # if carga > maximo or carga < 0:
+            #     contadorIguales[a[0]] += 1
+            #     if contadorIguales[a[0]] > 5:
+            #         Ns[a[0]] += 1
+            #         print(f'añado componente {a[0]}')
+            #         contadorIguales[a[0]] = 0
+            #     error = True
+            # elif carga < minimo and Ns[a[0]] > 1:
+            #     contadorIguales[a[0]] += 1
+            #     if contadorIguales[a[0]] > 5:
+            #         Ns[a[0]] -= 1
+            #         print(f'resto componente {a[0]}')
+            #         contadorIguales[a[0]] = 0
+            #     error = True
+
+            if carga > maximo or carga < 0:
+                accion['mas'] += 1
+            elif carga < minimo and N > 1:
+                accion['menos'] += 1
+
+        if accion['mas'] > 0:
+            N += 1
+            error = True
+        if accion['menos'] == len(cargas):
+            N -= 1
+            error = True
+        actualizaGrafico(2, 3, axes, valores)
+        if error:
+            g -= incGamma
+
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+def tRespMedio():
+    fig, axes, valores = grafico(2, 3)
+    N = 1
+    g = 0
+    maximo = 25
+    minimo = 10
+    iteracion = -1  # Iteración actual
+    incGamma = 0.2
+    contadorIguales = {}
+    for el in statesE:
+        contadorIguales[el] = 0
+    while iteracion < 1000:
+        incGamma = random.random() * 0.5
+
+        iteracion += 1
+        if iteracion > 100 and iteracion < 200:
+            incGamma = -1 * incGamma
+        if iteracion > 400 and iteracion < 600:
+            incGamma = -1 * incGamma
+        if iteracion > 900 and iteracion < 1000:
+            incGamma = -1 * incGamma
+        if g + incGamma < 0:
+            incGamma = 0
+
+        g += incGamma
+
+        error = False
+        # cargas = cargaPorGammaAndNumComponentes(g, Ns)
+        cargas = cargaPorGamma(g, N)
+        accion = {'mas': 0, 'menos': 0}
+        tResp = 0
+        for indElemento, a in enumerate(cargas):
+            carga = round(a[1] * 100, 2)
+            if indElemento > 2:
+                i = 1
+                j = indElemento - 3
+            else:
+                i = 0
+                j = indElemento
+
+            valores[i][j]['x'].append(iteracion)
+            valores[i][j]['y'].append(carga)
+            # valores[i][j]['nComp'] = Ns[a[0]]
+            valores[i][j]['nComp'] = N
+            valores[i][j]['gamma'] = str(round(g, 2)) + f' n={a[0]} K={carga}'
+            # print(valores)
+
+            # if carga > maximo or carga < 0:
+            #     contadorIguales[a[0]] += 1
+            #     if contadorIguales[a[0]] > 5:
+            #         Ns[a[0]] += 1
+            #         print(f'añado componente {a[0]}')
+            #         contadorIguales[a[0]] = 0
+            #     error = True
+            # elif carga < minimo and Ns[a[0]] > 1:
+            #     contadorIguales[a[0]] += 1
+            #     if contadorIguales[a[0]] > 5:
+            #         Ns[a[0]] -= 1
+            #         print(f'resto componente {a[0]}')
+            #         contadorIguales[a[0]] = 0
+            #     error = True
+
+            tResp += carga
+
+        if tResp / len(cargas) > maximo:
+            N += 1
+            error = True
+        if tResp / len(cargas) < minimo and N > 1:
+            N -= 1
+            error = True
+
+        actualizaGrafico(2, 3, axes, valores)
+        if error:
+            g -= incGamma
+
+    plt.show()
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     #s_value = Vx(V, dfA, 's')
-    
-    #gamma ->parametro perteneciente a la carga total que llega al sistema
+
+    # fig, axes, valores = grafico(2, 3)
+    # valores = [ [{'x': [0, 1, 2, 3], 'y': [0, 1, 2, 3], 'nComp': 1},   {'x': [0, 1, 2, 3], 'y': [-1, 5, 6, -2], 'nComp': 0},   {'x': [0, 1, 2, 3], 'y': [0, 0, 2, 0], 'nComp': 0}],
+    #             [{'x': [0, 1, 2, 3], 'y': [4, 5, 7, 8], 'nComp': 0},   {'x': [0, 1, 2, 3], 'y': [9, 7, 5, 2], 'nComp': 2},   {'x': [0, 1, 2, 3], 'y': [-1, 1, -1, 1], 'nComp': 0}]]
+    # print(valores[0][0]['x'])
+    # print(valores[0][0]['y'])
+    # print(valores[0][1]['x'])
+    # print(valores[0][1]['y'])
+    # print(valores[0][2]['x'])
+    # print(valores[0][2]['y'])
+    # actualizaGrafico(2, 3, axes, valores)
+    # exit()
+
+
+    #gamma -> parametro perteneciente a la carga total que llega al sistema
     gamma = 11
     
     #la fraccion de sesion del tipo A y B respectivamente
@@ -192,15 +462,15 @@ if __name__ == '__main__':
         rx.append(   (i, Rx(i, N, gamma))   )
    
    
-    print("obtención de las visitas: A")
+    print("Obtención de las visitas: A")
     print(V(dfA))
-    print("obtención de las visitas: B")
+    print("Obtención de las visitas: B")
     print(V(dfB))
-    print("obtención de las cargas")
+    print("Obtención de las cargas")
     print(hx)
-    print("obtención de las utilidades")
+    print("Obtención de las utilidades")
     print(ux)
-    print("obtención de los tiempos de respuesta")
+    print("Obtención de los tiempos de respuesta")
     print(rx)
     print()
     print()
@@ -209,8 +479,18 @@ if __name__ == '__main__':
     print()
     print()
     print()
-    for g in range(0, 100, 10):
-        print(g)
-        print(cargaPorGamma(g, 5))
+    # N = 1
+    # Ns = {}
+    # for el in statesE:
+    #     Ns[el] = 1
+    # g = 0
+    # maximo = 80
+    # minimo = 1
+
+
+    # tRespMedio()
+    tRespMaxMin()
+
+
 
 
